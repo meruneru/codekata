3abc20cbf1e788806f93430f67733f91de784fd8
meruneru <meruneru1032@gmail.com>
2021-12-16 23:02:25 +0900

    kata02 ans1

--- kata02/kata02.go
+++ kata02/kata02.go
@@ -2,12 +2,40 @@ package main
 
 import "fmt"
 
-func chop(index int32, ls []int32) int32 {
-	return -1
+// LiniearSearch
+// func chop(target int, ls []int) int {
+// 	for i, v := range ls {
+// 		if target == v {
+// 			return i
+// 		}
+// 	}
+// 	return -1
+// }
+
+// BinarySearch
+func chop(target int, ls []int) int {
+	start := 0
+	end := len(ls) - 1
+
+	for {
+		if start > end {
+			return -1
+		}
+		index := (start + end) / 2
+		if target == ls[index] {
+			return index
+		}
+
+		if ls[index] < target {
+			start = index + 1
+		} else {
+			end = index - 1
+		}
+	}
 }
 
 func main() {
-	list := []int32{1, 3, 5}
+	list := []int{1, 3, 5}
 
-	fmt.Println(chop(1, list))
+	fmt.Println(chop(3, list))
 }
--- kata02/kata02_test.go
+++ kata02/kata02_test.go
@@ -4,26 +4,66 @@ import (
 	"testing"
 )
 
-func TestChop_normalCase(t *testing.T) {
-	if chop(1, []int32{1, 3, 5}) != 0 {
+func TestChop_Case1(t *testing.T) {
+	if chop(3, []int{}) != -1 {
 		t.Fatal("faild!")
 	}
-	if chop(3, []int32{1, 3, 5}) != 1 {
+	if chop(3, []int{1}) != -1 {
 		t.Fatal("faild!")
 	}
-	if chop(5, []int32{1, 3, 5}) != 2 {
+	if chop(1, []int{1}) != 0 {
+		t.Fatal("faild!")
+	}
+}
+func TestChop_Case2(t *testing.T) {
+	if chop(1, []int{1, 3, 5}) != 0 {
+		t.Fatal("faild!")
+	}
+	if chop(3, []int{1, 3, 5}) != 1 {
+		t.Fatal("faild!")
+	}
+	if chop(5, []int{1, 3, 5}) != 2 {
+		t.Fatal("faild!")
+	}
+	if chop(0, []int{1, 3, 5}) != -1 {
+		t.Fatal("faild!")
+	}
+	if chop(2, []int{1, 3, 5}) != -1 {
+		t.Fatal("faild!")
+	}
+	if chop(4, []int{1, 3, 5}) != -1 {
+		t.Fatal("faild!")
+	}
+	if chop(6, []int{1, 3, 5}) != -1 {
 		t.Fatal("faild!")
 	}
 }
 func TestChop_abnormalCase(t *testing.T) {
-	if chop(0, []int32{1, 3, 5}) != -1 {
+	if chop(1, []int{1, 3, 5, 7}) != 0 {
 		t.Fatal("faild!")
 	}
-	if chop(0, []int32{}) != -1 {
+	if chop(3, []int{1, 3, 5, 7}) != 1 {
 		t.Fatal("faild!")
 	}
-	if chop(1, []int32{1, 1, 3, 5}) != 0 {
+	if chop(5, []int{1, 3, 5, 7}) != 2 {
 		t.Fatal("faild!")
 	}
-
+	if chop(7, []int{1, 3, 5, 7}) != 3 {
+		t.Fatal("faild!")
+	}
+	if chop(0, []int{1, 3, 5, 7}) != -1 {
+		t.Fatal("faild!")
+	}
+	if chop(2, []int{1, 3, 5, 7}) != -1 {
+		t.Fatal("faild!")
+	}
+	if chop(4, []int{1, 3, 5, 7}) != -1 {
+		t.Fatal("faild!")
+	}
+	if chop(6, []int{1, 3, 5, 7}) != -1 {
+		t.Fatal("faild!")
+	}
+	if chop(8, []int{1, 3, 5, 7}) != -1 {
+		t.Fatal("faild!")
+	}
 }
