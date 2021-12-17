d19c8d7b52dd7c9bb72cd307819e6a591b42d89f
meruneru <meruneru1032@gmail.com>
2021-12-17 12:39:34 +0900

    binarysearch v2

--- kata02/kata02.go
+++ kata02/kata02.go
@@ -12,30 +12,55 @@ import "fmt"
 // 	return -1
 // }
 
-// BinarySearch
-func chop(target int, ls []int) int {
-	start := 0
-	end := len(ls) - 1
+// BinarySearch v1
+// func chop(target int, ls []int) int {
+// 	start := 0
+// 	end := len(ls) - 1
+//
+// 	for {
+// 		if start > end {
+// 			return -1
+// 		}
+// 		index := (start + end) / 2
+// 		if target == ls[index] {
+// 			return index
+// 		}
+//
+// 		if ls[index] < target {
+// 			start = index + 1
+// 		} else {
+// 			end = index - 1
+// 		}
+// 	}
+// }
 
-	for {
-		if start > end {
+//BinarySearch v2
+func chop(target int, ls []int) (result int) {
+	mediam := len(ls) / 2
+
+	switch {
+	case len(ls) == 0:
+		result = -1
+	case ls[mediam] < target:
+		result = chop(target, ls[mediam+1:])
+		if result == -1 {
 			return -1
 		}
-		index := (start + end) / 2
-		if target == ls[index] {
-			return index
+		result += mediam + 1
+	case ls[mediam] > target:
+		result += mediam
+		result = chop(target, ls[:mediam])
+		if result == -1 {
+			return -1
 		}
-
-		if ls[index] < target {
-			start = index + 1
-		} else {
-			end = index - 1
-		}
+	default:
+		result = mediam
 	}
+	return
 }
 
 func main() {
-	list := []int{1, 3, 5}
+	list := []int{3}
 
-	fmt.Println(chop(3, list))
+	fmt.Println(chop(5, list))
 }
